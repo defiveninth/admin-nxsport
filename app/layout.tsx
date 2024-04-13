fdfe1a3744410c2743baa48d561603212d2db690
dearm762 <misterfighter1990@gmail.com>
2024-04-13 08:22:06 +0600

    form ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import '@/styles/globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: 'Narxoz Sport | Главная',
+  title: 'Narxoz Sport',
 }
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,13 @@
 import { FC } from 'react'
-import Image from 'next/image'
-import type { Metadata } from 'next'
-
 import AuthForm from '@/components/auth-form'
+import S from '@/styles/main-page.module.css'
 
-import S from '@/styles/home.module.css'
-import toInside from '@/acion/getInside'
-
-export const metadata: Metadata = {
-	title: 'Narxoz Sport | Войти',
-}
-
-const Home: FC = async () => {
-	await toInside()
+const Home: FC = () => {
 	return (
-		<main className={S.main}>
-			<div className={S.red}>
-				<Image alt='nx' src={'/nx-logo.png'} width={80} height={80}></Image>
-			</div>
-			<div className={S.wrapper}>
-				<h1 className={S.formHeader}>Narxoz Sport | Войти</h1>
-				<AuthForm />
-			</div>
-		</main>
+		<div className={ S.wrapper }>
+			<h2>Нархоз Спорт | Админ</h2>
+			<AuthForm />
+		</div>
 	)
 }
 
