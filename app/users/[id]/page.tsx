85e6fd91df862e33fb6ee2ed169bb7018e2bb319
dearm762 <misterfighter1990@gmail.com>
2024-04-18 22:46:42 +0600

    404 page ready

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -8,68 +8,75 @@ import CurrentRoute from '@/components/current-route'
 import IRoute from '@/types/route'
 import { CircleUserRound, GraduationCap } from 'lucide-react'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation';
 
 const UserProfile: FC<IUserProfileProps> = ({ params: { id } }) => {
-	const [userData, setUserData] = useState<User>();
-	
+  const [userData, setUserData] = useState<User>();
+  const router = useRouter();
+
   useEffect(() => {
-		const fetchUserData = async () => {
-			try {
-				const response = await fetch('http://localhost:3001/users/get-user-data', {
-					method: 'POST',
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/users/get-user-data', {
+          method: 'POST',
           headers: {
-						'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({ id: id }),
-        });
-				
+        })
+
         if (!response.ok) {
-					throw new Error('Failed to fetch user data');
+          const errorData = await response.json();
+          if (response.status === 404 && errorData.message === 'Юзер не найден') {
+            router.push('/users');
+          } else {
+            throw new Error('Failed to fetch user data');
+          }
         }
-				
+
         const userData = await response.json();
         setUserData(userData);
       } catch (error) {
-				console.error('Error fetching user data:', error);
+        console.error('Error fetching user data:', error);
       }
     };
-		
+
     fetchUserData();
-  }, [id]);
-	
-	const route: Array<IRoute> = [
-		{
-			icon: GraduationCap,
-			route: 'Студенты',
-			url: '/students'
-		},
-		{
-			icon: CircleUserRound,
-			route: `${userData?.first_name} ${userData?.last_name}`,
-			url: `/users/${id}`
-		},
-	]
+  }, [id, router]);
+
+  const route: Array<IRoute> = [
+    {
+      icon: GraduationCap,
+      route: 'Студенты',
+      url: '/students'
+    },
+    {
+      icon: CircleUserRound,
+      route: `${userData?.first_name} ${userData?.last_name}`,
+      url: `/users/${id}`
+    },
+  ]
 
   return (
     <>
       <Nav now='students' />
-			<main className='ml-[320px]'>
-				<CurrentRoute route={ route } />
-				<div className='bg-red-50 mr-5 p-5 rounded-2xl flex items-center gap-10'>
-					<img src={ userData?.profile_photo } alt='userphoto' className='w-48 h-48 rounded-full border-4 border-solid border-red-500 p-1' />
-					<div className='flex flex-col gap-1'>
-						<h2 className='text-3xl font-semibold'>{ userData?.first_name } { userData?.last_name }</h2>
-						<span>{ userData?.birth_date.split('-').reverse().join('-') }</span>
-						<Link href={`https://wa.me/${userData?.phone_number.replace(
-									/\s|\(|\)/g,
-									''
-								)}`}
-								target='_blank' className='link link-primary'>{ userData?.phone_number }</Link>
-						<Link href={`mailto:${userData?.email}`} className='link link-primary'>{ userData?.email }</Link>
-					</div>
-				</div>
-      <pre>{JSON.stringify(userData, null, 2)}</pre>
-			</main>
+      <main className='ml-[320px]'>
+        <CurrentRoute route={route} />
+        <div className='bg-red-50 mr-5 p-5 rounded-2xl flex items-center gap-10'>
+          <img src={userData?.profile_photo} alt='userphoto' className='w-48 h-48 rounded-full border-4 border-solid border-red-500 p-1' />
+          <div className='flex flex-col gap-1'>
+            <h2 className='text-3xl font-semibold'>{userData?.first_name} {userData?.last_name}</h2>
+            <span>{userData?.birth_date.split('-').reverse().join('-')}</span>
+            <Link href={`https://wa.me/${userData?.phone_number.replace(
+              /\s|\(|\)/g,
+              ''
+            )}`}
+              target='_blank' className='link link-primary'>{userData?.phone_number}</Link>
+            <Link href={`mailto:${userData?.email}`} className='link link-primary' target='_blank'>{userData?.email}</Link>
+          </div>
+        </div>
+        <pre>{JSON.stringify(userData, null, 2)}</pre>
+      </main>
     </>
   );
 };
---
+++ app/users/page.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react'
+import CurrentRoute from '@/components/current-route'
+import Nav from '@/components/nav'
+import IRoute from '@/types/route'
+import { ShieldAlert, Users } from 'lucide-react'
+import Link from 'next/link'
+
+const route: Array<IRoute> = [
+	{
+		icon: Users,
+		route: 'Пользователей',
+		url: '/users',
+	},
+  {
+		icon: ShieldAlert,
+		route: '404 Ошибка',
+		url: '/users',
+	}
+]
+
+
+
+const NotFound: FC = () => {
+  return (
+    <>
+      <Nav now='students' />
+      <main className='ml-[320px]'>
+        <CurrentRoute route={route} />
+        <h2 className='text-center text-5xl font-bold mt-32'>404</h2>
+        <h2 className='text-center font-semibold text-xl mt-5'>Этот пользователь не найден</h2>
+        <h2 className='text-center mt-5'>
+        <Link href={'/'} className='btn btn-outline btn-error mx-auto text-center px-16'>Домой</Link>
+        </h2>
+      </main>
+    </>
+  )
+}
+
+export default NotFound
