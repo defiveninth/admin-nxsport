2126ecf8a4f2a129dc5dee52a93be71c5006c1ac
dearm762 <misterfighter1990@gmail.com>
2024-04-20 19:03:31 +0600

    created sections client page

--- app/sections/page.tsx
+++ app/sections/page.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react'
 import type { Metadata } from 'next'
+import { BookA } from 'lucide-react'
 import Nav from '@/components/nav'
 import CurrentRoute from '@/components/current-route'
+import SectionsPage from '@/screens/sections'
 import IRoute from '@/types/route'
-import { BookA } from 'lucide-react'
 
 export const metadata: Metadata = {
   title: 'Нархоз Спорт | Секций',
@@ -23,6 +24,7 @@ const Controll: FC = () => {
 			<Nav now='sections' />
 			<main className='ml-[320px]'>
 				<CurrentRoute route={ route } />
+				<SectionsPage />
 			</main>
 		</>
 	)
---
+++ screens/sections.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { FC } from 'react'
+
+const SectionsPage: FC = () => {
+	return (
+		<div>SectionsPage</div>
+	)
+}
+
+export default SectionsPage
