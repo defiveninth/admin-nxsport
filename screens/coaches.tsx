7f1c05a4cdbde85cce3549ddf6e7f6767299a40f
dearm762 <misterfighter1990@gmail.com>
2024-05-03 09:39:16 +0600

    created coach phone and em

--- screens/coaches.tsx
+++ screens/coaches.tsx
@@ -4,6 +4,7 @@ import { FC, useEffect, useState } from 'react'
 import Link from 'next/link'
 import Trainer from '@/types/trainer'
 import User from '@/types/user'
+import { Mail, SendHorizontal } from 'lucide-react'
 
 const CoachesPage: FC = () => {
 	const [trainers, setTrainers] = useState<Array<Trainer>>()
@@ -59,9 +60,39 @@ const CoachesPage: FC = () => {
 						{trainers.map(trainer => {
 							const info = trainerInfo.find(info => info.id === trainer.user_id)
 							return (
-								<div key={trainer.id} className='bg-red-50 p-5 rounded-2xl flex items-center mr-5'>
-									<img src={ info?.profile_photo } alt={ info?.first_name } width={50} height={50} className='mr-5 rounded-full' />
-									<Link href={ `/users/${info?.id}` }>{ info?.first_name } { info?.last_name }</Link>
+								<div
+									key={trainer.id}
+									className='bg-red-50 p-5 rounded-2xl items-center mr-5 min-w-64 flex gap-5'
+								>
+									<img
+										src={info?.profile_photo}
+										alt={info?.first_name}
+										width={50}
+										height={50}
+										className='mr-5 rounded-full'
+									/>
+									<Link href={`/coaches/${info?.id}`}>
+										{info?.first_name} {info?.last_name}: {info?.type_section === 0 ? 'Обычный' : 'Лечебный'}
+									</Link>
+									<Link
+										href={`https://wa.me/${info?.phone_number.replace(
+											/\s|\(|\)/g,
+											''
+										)}`}
+										target='_blank'
+										className='mr-20 text-blue-500 font-medium flex gap-2 items-center hover:underline'
+									>
+										{info?.phone_number}
+										<SendHorizontal width={20} height={20} />
+									</Link>
+									<Link
+										href={`mailto:${info?.email}`}
+										target='_blank'
+										className='text-blue-500 font-medium flex gap-2 items-center hover:underline'
+									>
+										{info?.email}
+										<Mail width={20} height={20} />
+									</Link>
 								</div>
 							)
 						})}
